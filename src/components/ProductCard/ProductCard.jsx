b632475f0be23a906c6142a2463849ed815c2775
Nikhil Agarwal <agarwalnikhil576@gmail.com>
2022-10-14 20:52:41 +0530

    dark mode

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,8 @@ import { getProductById } from "../../utils/findProductInCart";
 import { useNavigate } from "react-router-dom";
 
 export const ProductCard = (props) => {
-  const { id, name, image, oldPrice, newPrice, rating, discount } = props.product;
+  const { id, name, image, oldPrice, newPrice, rating, discount } =
+    props.product;
 
   const { cart, cartDispatch } = useCart();
 
@@ -24,26 +25,53 @@ export const ProductCard = (props) => {
   };
 
   return (
-    <div className={`card card-vertical d-flex direction-column relative shadow bg-${props.mode}`}>
+    <div
+      className={`card card-vertical d-flex direction-column relative shadow bg-${props.mode}`}
+    >
       <div className="card-image-container">
         <img className="card-image" src={image} alt="shoes" />
       </div>
       <div className="card-details">
-        <div className={`card-title text-${props.mode==="light" ? "dark" : "light"}`}>{name}</div>
+        <div
+          className={`card-title text-${
+            props.mode === "light" ? "dark" : "light"
+          }`}
+        >
+          {name}
+        </div>
         <div className="card-description">
           <p className="card-price d-flex align-end">
-            <span className={`new-price text-${props.mode==="light" ? "dark" : "light"}`}>Rs. {newPrice}</span>
-            <span className={`price-strike-through text-${props.mode==="light" ? "dark" : "light"}`}>Rs. {oldPrice}</span>
+            <span
+              className={`new-price text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              Rs. {newPrice}
+            </span>
+            <span
+              className={`price-strike-through text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              Rs. {oldPrice}
+            </span>
             <span className="discount">({discount}% OFF)</span>
           </p>
-          <p className={`d-flex align-center text-${props.mode==="light" ? "dark" : "light"}`}>
+          <p
+            className={`d-flex align-center text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             <span className="star material-icons-outlined">star</span>
             {rating}
           </p>
         </div>
-        <div className="cta-btn">
+
+        <div class="d-grid gap-2">
           <button
-            className="button btn-primary btn-icon cart-btn d-flex align-center justify-center gap cursor btn-margin"
+            className={`btn btn-dark ${
+              props.mode === "light" ? "" : "btn-outline-light"
+            }`}
             onClick={
               isProductInCart ? handleGoToCartClick : handleAddToCartClick
             }
--- src/components/ProductCard/ProductCardHorizontal.jsx
+++ src/components/ProductCard/ProductCardHorizontal.jsx
@@ -1,7 +1,8 @@
 import { useCart } from "../../context/cart-context";
 
 export const ProductCardHorizontal = (props) => {
-  const { id, name, image, oldPrice, newPrice, discount, quantity } = props.product;
+  const { id, name, image, oldPrice, newPrice, discount, quantity } =
+    props.product;
 
   const { cartDispatch } = useCart();
 
@@ -27,33 +28,71 @@ export const ProductCardHorizontal = (props) => {
   };
 
   return (
-    <div className={`card-horizontal d-flex shadow my-2 bg-${props.mode==='light' ? "light" : "dark"}`} id="myCard">
+    <div
+      className={`card-horizontal d-flex shadow my-2 bg-${
+        props.mode === "light" ? "light" : "dark"
+      }`}
+      id="myCard"
+    >
       <div className="card-hori-image-container">
         <img className="card-image" src={image} alt="Book" />
       </div>
       <div className="card-details d-flex direction-column">
-        <div className={`card-title text-${props.mode==='light' ? "dark" : "light"}`}>{name}</div>
+        <div
+          className={`card-title text-${
+            props.mode === "light" ? "dark" : "light"
+          }`}
+        >
+          {name}
+        </div>
         <div className="card-description">
-          <p className={`card-price text-${props.mode==='light' ? "dark" : "light"}`}>
+          <p
+            className={`card-price text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             Rs. {newPrice}
-            <span className={`price-strike-through padding-all-8 text-${props.mode==='light' ? "dark" : "light"}`}>
+            <span
+              className={`price-strike-through padding-all-8 text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
               Rs. {oldPrice}
             </span>
             <span className="discount padding-all-8">({discount}% OFF)</span>
           </p>
         </div>
         <div className="quantity-container d-flex gap">
-          <p className={`q-title text-${props.mode==='light' ? "dark" : "light"}`}>Quantity: </p>
+          <p
+            className={`q-title text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            Quantity:{" "}
+          </p>
           <div className="count-container d-flex align-center gap">
             <button
-              className={`count text-${props.mode==='light' ? "dark" : "light"}`}
+              className={`count text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
               onClick={() => handleDecrement(id)}
               disabled={quantity === 1}
             >
               -
             </button>
-            <span className={`count-value text-${props.mode==='light' ? "dark" : "light"}`}>{quantity}</span>
-            <button className={`count text-${props.mode==='light' ? "dark" : "light"}`} onClick={() => handleIncrement(id)}>
+            <span
+              className={`count-value text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              {quantity}
+            </span>
+            <button
+              className={`count text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+              onClick={() => handleIncrement(id)}
+            >
               +
             </button>
           </div>
@@ -61,31 +100,29 @@ export const ProductCardHorizontal = (props) => {
 
         <div className="cta-btn d-flex gap">
           <div className="cta-btn">
-            <button type="button" onClick={() => handleRemoveFromCartClick(id)} className={`button hori-btn btn-primary btn-icon d-flex align-center justify-center gap cursor btn-margin`}>
+            <button
+              type="button"
+              onClick={() => handleRemoveFromCartClick(id)}
+              className={`button hori-btn btn-outline-${
+                props.mode === "light" ? "" : "secondary"
+              } btn-primary btn-icon d-flex align-center justify-center gap cursor btn-margin`}
+            >
               {" "}
               Remove From Cart
             </button>
           </div>
           <div className="cta-btn">
-            <button className={`button hori-btn btn-outline-primary btn-icon d-flex align-center justify-center gap cursor btn-margin text-${props.mode==='light' ? "dark" : "light"}`}>
+            <button
+              className={`button hori-btn btn-outline-${
+                props.mode === "light" ? "primary" : "secondary"
+              } btn-icon d-flex align-center justify-center gap cursor btn-margin text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
               Move to ❤️
             </button>
           </div>
         </div>
-
-        {/* <div className="cta-btn d-flex gap">
-          <div className="cta-btn">
-            <button type="button" onClick={() => handleRemoveFromCartClick(id)} className={` button hori-btn btn-primary btn-icon d-flex align-center justify-center gap cursor btn-margin ${props.mode==='light' ? "" : "btn-outline-light"}`}>
-              {" "}
-              Remove From Cart
-            </button>
-          </div>
-          <div className="cta-btn">
-            <button className={`btn button hori-btn btn-outline-${props.mode==='light' ? "primary" : "light"} btn-icon d-flex align-center justify-center gap cursor btn-margin text-${props.mode==='light' ? "dark" : "light"}`}>
-              Move to ❤️
-            </button>
-          </div>
-        </div> */}
       </div>
     </div>
   );
